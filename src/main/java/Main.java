c7fc1b4eacb4c34ce423c7fd792c172f9c901871
Evgeniia Togochakova <lookatma@mail.ru>
2023-07-05 12:37:30 +0700

    Added 2 teams to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,55 +1,83 @@
-// Проанализировать и описать персонажей:
-// Маг, монах, разбойник, копейщик, снайпер, арбалетчик, крестьянин.
-// На базе описания персонажей описать простейшую иерархию классов.
-// В основной программе создать по одному экземпляру каждого класса.
+// Добавить файл с описанием интерфейса.
+// В котором описать два метода, void step(); и String getInfo();
+// Реализовать интерфейс в абстрактном классе и в наследниках так, чтобы getInfo возвращал тип персонажа.
+// Создать два списка в классе main.
+// В каждый из списков добавить по десять случайных экземнляров наследников BaseHero.
+// Удалить ненужные методы из абстрактного класса, если такие есть.
+// В main пройти по спискам и вызвать у всех персонажей getInfo.
 
 import units.*;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        Magician magician = new Magician();
-        System.out.println(magician.about());
-        Monk monk = new Monk();
-        System.out.println(monk.about());
-        Robber robber = new Robber();
-        System.out.println(robber.about());
-        Spearman spearman = new Spearman();
-        System.out.println(spearman.about());
-        Sniper sniper = new Sniper();
-        System.out.println(sniper.about());
-        Crossbowman crossbowman = new Crossbowman();
-        System.out.println(crossbowman.about());
-        Peasant peasant = new Peasant();
-        System.out.println(peasant.about());
-        magician.toAttack(peasant);
-        System.out.println(magician.about());
-        System.out.println(peasant.about());
-        spearman.toAttack(magician);
-        System.out.println(spearman.about());
-        System.out.println(magician.about());
-        spearman.toAttack(magician);
-        System.out.println(spearman.about());
-        System.out.println(magician.about());
-        spearman.toAttack(magician);
-        System.out.println(spearman.about());
-        System.out.println(magician.about());
-        monk.healing(magician);
-        magician.healing(magician);
-        System.out.println(magician.about());
+        ArrayList<Unit> teamWhiteRose = new ArrayList<>();
+        ArrayList<Unit> teamRedRose = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int digit = new Random().nextInt(7);
+            switch (digit) {
+                case 0 -> {
+                    teamWhiteRose.add(new Monk());
+                    teamRedRose.add(new Monk());
+                }
+                case 1 -> {
+                    teamWhiteRose.add(new Magician());
+                    teamRedRose.add(new Magician());
+                }
+                case 2 -> {
+                    teamWhiteRose.add(new Peasant());
+                    teamRedRose.add(new Peasant());
+                }
+                case 3 -> {
+                    teamWhiteRose.add(new Crossbowman());
+                    teamRedRose.add(new Crossbowman());
+                }
+                case 4 -> {
+                    teamWhiteRose.add(new Sniper());
+                    teamRedRose.add(new Sniper());
+                }
+                case 5 -> {
+                    teamWhiteRose.add(new Spearman());
+                    teamRedRose.add(new Spearman());
+                }
+                case 6 -> {
+                    teamWhiteRose.add(new Robber());
+                    teamRedRose.add(new Robber());
+                }
+            }
+        }
 
+        int archers = 0;
+        int peasants = 0;
 
+        // чтобы не оказалось так, что лучники есть в командах, а крестьян им в помощь нет.
+        // в таком случае придется добавить по крестьянину в команды (по одиннадцатому игроку)
 
+        for (int i = 0; i < teamWhiteRose.size(); i++) {
+            if (teamWhiteRose.get(i) instanceof Crossbowman) {
+                archers++;
+            }
+            if (teamWhiteRose.get(i) instanceof Peasant) {
+                peasants++;
+            }
+        }
 
+        System.out.println("Лучников по " + archers + " в каждой из команд");
+        System.out.println("Крестьян по " + peasants + " в каждой из команд");
 
+        if ((archers > 0)&&(peasants == 0)) {
+            teamWhiteRose.add(new Peasant());
+            teamRedRose.add(new Peasant());
+            System.out.println("Пришлось добавить по одному крестьянину в команды сверх нормы");
+        }
 
+        for (int i = 0; i < teamWhiteRose.size(); i++) {
+            System.out.println(teamWhiteRose.get(i).getInfo());
+            System.out.println(teamRedRose.get(i).getInfo());
+        }
 
-
-
-
-
-
-
-
-
+        System.out.println("Общее количество игроков в каждой из команд в итоге составило " + teamWhiteRose.size());
     }
 }
