c8ae5cef5d11148ce63e43194d6923a720c43047
Evgeniia Togochakova <lookatma@mail.ru>
2023-07-06 14:37:01 +0700

    Enhanced Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,7 @@
-// Добавить файл с описанием интерфейса.
-// В котором описать два метода, void step(); и String getInfo();
-// Реализовать интерфейс в абстрактном классе и в наследниках так, чтобы getInfo возвращал тип персонажа.
-// Создать два списка в классе main.
-// В каждый из списков добавить по десять случайных экземнляров наследников BaseHero.
-// Удалить ненужные методы из абстрактного класса, если такие есть.
-// В main пройти по спискам и вызвать у всех персонажей getInfo.
+//1.Создать класс с описанием координат, x и y.
+//2.Добавить в абстрактный класс поле с координатами и пробросить его инициализацию до конструкторов персонажей.
+//Farmer farmer = new Farmer(getName(), x, y);
+//3.Найти среди противников ближайшего и вывести расстояние до него и его имя в консоль.
 
 import units.*;
 
@@ -15,36 +12,38 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Unit> teamWhiteRose = new ArrayList<>();
         ArrayList<Unit> teamRedRose = new ArrayList<>();
+
+
         for (int i = 0; i < 10; i++) {
             int digit = new Random().nextInt(7);
             switch (digit) {
                 case 0 -> {
-                    teamWhiteRose.add(new Monk());
-                    teamRedRose.add(new Monk());
+                    teamWhiteRose.add(new Monk(new Random().nextInt(0, 6), new Random().nextInt(51)));
+                    teamRedRose.add(new Monk(new Random().nextInt(20, 26), new Random().nextInt(51)));
                 }
                 case 1 -> {
-                    teamWhiteRose.add(new Magician());
-                    teamRedRose.add(new Magician());
+                    teamWhiteRose.add(new Magician(new Random().nextInt(0, 6), new Random().nextInt(51)));
+                    teamRedRose.add(new Magician(new Random().nextInt(20, 26), new Random().nextInt(51)));
                 }
                 case 2 -> {
-                    teamWhiteRose.add(new Peasant());
-                    teamRedRose.add(new Peasant());
+                    teamWhiteRose.add(new Peasant(new Random().nextInt(0, 6), new Random().nextInt(51)));
+                    teamRedRose.add(new Peasant(new Random().nextInt(20, 26), new Random().nextInt(51)));
                 }
                 case 3 -> {
-                    teamWhiteRose.add(new Crossbowman());
-                    teamRedRose.add(new Crossbowman());
+                    teamWhiteRose.add(new Crossbowman(new Random().nextInt(0, 6), new Random().nextInt(51)));
+                    teamRedRose.add(new Crossbowman(new Random().nextInt(20, 26), new Random().nextInt(51)));
                 }
                 case 4 -> {
-                    teamWhiteRose.add(new Sniper());
-                    teamRedRose.add(new Sniper());
+                    teamWhiteRose.add(new Sniper(new Random().nextInt(0, 6), new Random().nextInt(51)));
+                    teamRedRose.add(new Sniper(new Random().nextInt(20, 26), new Random().nextInt(51)));
                 }
                 case 5 -> {
-                    teamWhiteRose.add(new Spearman());
-                    teamRedRose.add(new Spearman());
+                    teamWhiteRose.add(new Spearman(new Random().nextInt(0, 6), new Random().nextInt(51)));
+                    teamRedRose.add(new Spearman(new Random().nextInt(20, 26), new Random().nextInt(51)));
                 }
                 case 6 -> {
-                    teamWhiteRose.add(new Robber());
-                    teamRedRose.add(new Robber());
+                    teamWhiteRose.add(new Robber(new Random().nextInt(0, 6), new Random().nextInt(51)));
+                    teamRedRose.add(new Robber(new Random().nextInt(20, 26), new Random().nextInt(51)));
                 }
             }
         }
@@ -67,9 +66,9 @@ public class Main {
         System.out.println("Лучников по " + archers + " в каждой из команд");
         System.out.println("Крестьян по " + peasants + " в каждой из команд");
 
-        if ((archers > 0)&&(peasants == 0)) {
-            teamWhiteRose.add(new Peasant());
-            teamRedRose.add(new Peasant());
+        if ((archers > 0) && (peasants == 0)) {
+            teamWhiteRose.add(new Peasant(new Random().nextInt(0, 6), new Random().nextInt(51)));
+            teamRedRose.add(new Peasant(new Random().nextInt(20, 26), new Random().nextInt(51)));
             System.out.println("Пришлось добавить по одному крестьянину в команды сверх нормы");
         }
 
@@ -79,5 +78,12 @@ public class Main {
         }
 
         System.out.println("Общее количество игроков в каждой из команд в итоге составило " + teamWhiteRose.size());
+
+        teamWhiteRose.forEach(n -> {
+            String[] strings = n.getInfo().split(" ");
+            System.out.print("Для " + strings[1] + " ближайший враг и расстояние до него - ");
+            n.step(teamRedRose);
+        });
+
     }
 }
