b332914c2efffc592816df570764c0deb4119172
Evgeniia Togochakova <lookatma@mail.ru>
2023-07-09 00:35:30 +0700

    Changed Crossbowman.java

--- src/main/java/units/Crossbowman.java
+++ src/main/java/units/Crossbowman.java
@@ -7,7 +7,7 @@ public class Crossbowman extends Unit {
     int arrows;
 
     public Crossbowman(int x, int y) {
-        super(100, 50, 25, 25, new int[]{5, 10, 15}, x, y );
+        super(100, 50, new Random().nextInt(25, 101), 25, new int[]{5, 10, 15}, x, y);
         this.arrows = new Random().nextInt(10, 20);
     }
 
@@ -18,33 +18,41 @@ public class Crossbowman extends Unit {
         return false;
     }
 
+
     @Override
-    protected boolean attackAbility() {
-        if (this.attack >= 5 && !needArrows()) return true;
-        return false;
+    public void toAttack(Unit target) {
+        this.attack -= 5;
+        System.out.println("Атакующих сил у "+ name + " стало меньше, теперь "+ attack);
+        target.getDamageByArcher();
+        System.out.println("У жертвы " + target.name + " в результате атаки уровень жизненных сил понизился до " + target.currentHp );
     }
 
-    public void toAttack(Unit target) {
-        if (needArrows()) {
-            System.out.println("Арбалетчик " + name + " стрелять не может, закончились стрелы");
+    @Override
+    public void step(ArrayList<Unit> heroes, ArrayList<Unit> myOwnTeam) {
+        Unit closestVictim = findClosestEnemy(heroes);
+        System.out.println(closestVictim.name + " " + this.coordinates.distanceСalculation(closestVictim.coordinates));
+        if (currentHp <= 0.1) {
+            System.out.println("Арбалетчик " + name + " уже не живой!");
+            return;
         }
         if (this.attack < 5) {
             System.out.println("Арбалетчик " + name + " уже исчерпал свои атакующие силы!");
+            return;
         }
-
-        if (attackAbility()) {
-            target.getDamage();
-            this.attack -= 5;
-            this.arrows -= 1;
-            System.out.println("Арбалетчик " + name + " метко выстрелил из арбалета в " + target.name);
+        if (needArrows()) {
+            System.out.println("Арбалетчик " + name + " стрелять не может, закончились стрелы");
+            return;
         }
+        Unit enemyTarget = findClosestEnemy(heroes);
+        System.out.println("Арбалетчик " + name + " метко выстрелил из арбалета в " + enemyTarget.name);
+        toAttack(enemyTarget);
+        System.out.println();
+        if (myOwnTeam.contains(Peasant.class)) {
+            return;
+        }
+        this.arrows--;
     }
 
-    @Override
-    public void step(ArrayList<Unit> heroes) {
-        Unit closestVictim = findClosestEnemy(heroes);
-        System.out.println(closestVictim.name + " " + this.coordinates.distanceСalculation(closestVictim.coordinates));
-    }
 
     @Override
     public String getInfo() {
