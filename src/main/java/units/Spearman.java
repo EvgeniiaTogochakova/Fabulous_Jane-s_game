0ae32e852438c474a86de2c6f650f12414848399
Evgeniia Togochakova <lookatma@mail.ru>
2023-07-09 00:38:52 +0700

    Changed Spearman.java

--- src/main/java/units/Spearman.java
+++ src/main/java/units/Spearman.java
@@ -1,15 +1,16 @@
 package units;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Spearman extends Unit {
 
     public Spearman(int x, int y) {
-        super(100, 50, 25, 25, new int[]{1, 2, 3, 5}, x,y);
+        super(100, 50, new Random().nextInt(25,101), 25, new int[]{1, 2, 3, 5, 9}, x,y);
     }
 
     @Override
-    public void step(ArrayList<Unit> heroes) {
+    public void step(ArrayList<Unit> heroes,ArrayList<Unit> myOwnTeam) {
         Unit closestVictim = findClosestEnemy(heroes);
         System.out.println(closestVictim.name + " " + this.coordinates.distanceСalculation(closestVictim.coordinates));
     }
