32d5ba8dabab7279201ed84bb2c4ae92fedf4603
Evgeniia Togochakova <lookatma@mail.ru>
2023-07-09 00:35:11 +0700

    Changed GameInterface.java

--- src/main/java/units/GameInterface.java
+++ src/main/java/units/GameInterface.java
@@ -3,6 +3,6 @@ package units;
 import java.util.ArrayList;
 
 public interface GameInterface {
-    void step(ArrayList<Unit> heroes);
+    void step(ArrayList<Unit> heroes, ArrayList<Unit> myOwnTeam);
     String getInfo();
 }
