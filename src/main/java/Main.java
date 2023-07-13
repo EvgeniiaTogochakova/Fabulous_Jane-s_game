7972a3de433ee17024b5a77d615ae6a3fdcd9eb7
Evgeniia Togochakova <lookatma@mail.ru>
2023-07-13 20:06:47 +0700

    Improved Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,119 +1,92 @@
-// Step лучников
-//1.Если жизни 0 вернуть управление
-//2.Если стрел 0 вернуть управление
-//3.Найти ближайшего противника
-//4.Нанести ему среднее повреждение
-//5.Если среди своих есть крестьянин вернуть управление
-//6.уменьшить кол-во стрел на одну и вернуть управление
-//Вызывать персонажей из обеих команд в порядке инициативы.
+//Реализовать визуализацию своего проекта по примеру семинара с использованием приложенных классов.
+//Доработать степ крестьян и магов!
 
-
-//В моей интерпретации игры я использую вместо "инициативы" другое слово - "атакующие силы". У лучников это называется
-//силами для стрельбы, у копейщика - силы для удара копьем, у разбойника - силы для атаки, у мага - чары для ослабления,
-//у монаха силы для распыления яда. В моей интерпретации игры при атаке не только жертва теряет Hp, но и атакующие силы
-//у атакующего быстро истощаются, со временем, будучи еще живым, атаковать он не сможет. Потом посмотрю, хорошая ли это практика.
-//Специально под лучников сделала метод getDamageByArcher(), где как раз-таки среднее арифметическое считается и
-//не учитывается наличие брони. Т.к. такое домашнее задание.
-//У меня есть getDamage() для общего случая, но он пока совсем другой.
-//Сначала вывожу общий состав команд по порядку, потом делаю step, сортируя по объему атакующих сил, т.е. инициативы.
+//В моей интерпретации игры маги и монахи сами решают, атаковать им или лечить в их step. Но если они решили все же лечить,
+//а лечить пока особо никого не надо в начале игры, тогда они могут и атаковать, чтобы зря не пропадал их ход. Если лечить надо,
+//действуют по-честному: лечат того соратника, который сильнее пострадал.
+//Оказалось, на поле 10*10 трудно расставить персонажей, чтобы всех было хорошо видно, если заполнять команды рандомно.
+// Некоторые игроки стоят на том же месте, что и их соратники. Правда, таких немного.
+//У меня монахи берут вторую букву названия класса - O, как и снайперы - N.
 
 
 
 import units.*;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Random;
+import java.util.Scanner;
 
 import static java.util.Collections.reverseOrder;
 import static java.util.Comparator.comparing;
 
 public class Main {
-    public static void main(String[] args) {
-        ArrayList<Unit> teamWhiteRose = new ArrayList<>();
-        ArrayList<Unit> teamRedRose = new ArrayList<>();
+    public static ArrayList<Unit> teamWhiteRose = new ArrayList<>();
+    public static ArrayList<Unit> teamRedRose = new ArrayList<>();
+    public static ArrayList<Unit> commonList = new ArrayList<>();
 
+    public static void main(String[] args) {
 
         for (int i = 0; i < 10; i++) {
             int digit = new Random().nextInt(7);
             switch (digit) {
                 case 0 -> {
-                    teamWhiteRose.add(new Monk(new Random().nextInt(0, 6), new Random().nextInt(51)));
-                    teamRedRose.add(new Monk(new Random().nextInt(20, 26), new Random().nextInt(51)));
+                    teamWhiteRose.add(new Monk(Generator.numberXWhiteRose(), Generator.numberYMonk()));
+                    teamRedRose.add(new Monk(Generator.numberXRedRose(), Generator.numberYMonk()));
                 }
                 case 1 -> {
-                    teamWhiteRose.add(new Magician(new Random().nextInt(0, 6), new Random().nextInt(51)));
-                    teamRedRose.add(new Magician(new Random().nextInt(20, 26), new Random().nextInt(51)));
+                    teamWhiteRose.add(new Magician(Generator.numberXWhiteRose(), Generator.numberYMagician()));
+                    teamRedRose.add(new Magician(Generator.numberXRedRose(), Generator.numberYMagician()));
                 }
                 case 2 -> {
-                    teamWhiteRose.add(new Peasant(new Random().nextInt(0, 6), new Random().nextInt(51)));
-                    teamRedRose.add(new Peasant(new Random().nextInt(20, 26), new Random().nextInt(51)));
+                    teamWhiteRose.add(new Peasant(Generator.numberXWhiteRose(), Generator.numberYPeasant()));
+                    teamRedRose.add(new Peasant(Generator.numberXRedRose(), Generator.numberYPeasant()));
                 }
                 case 3 -> {
-                    teamWhiteRose.add(new Crossbowman(new Random().nextInt(0, 6), new Random().nextInt(51)));
-                    teamRedRose.add(new Crossbowman(new Random().nextInt(20, 26), new Random().nextInt(51)));
+                    teamWhiteRose.add(new Crossbowman(Generator.numberXWhiteRose(), Generator.numberYCrossbowman()));
+                    teamRedRose.add(new Crossbowman(Generator.numberXRedRose(), Generator.numberYCrossbowman()));
                 }
                 case 4 -> {
-                    teamWhiteRose.add(new Sniper(new Random().nextInt(0, 6), new Random().nextInt(51)));
-                    teamRedRose.add(new Sniper(new Random().nextInt(20, 26), new Random().nextInt(51)));
+                    teamWhiteRose.add(new Sniper(Generator.numberXWhiteRose(), Generator.numberYSniper()));
+                    teamRedRose.add(new Sniper(Generator.numberXRedRose(), Generator.numberYSniper()));
                 }
                 case 5 -> {
-                    teamWhiteRose.add(new Spearman(new Random().nextInt(0, 6), new Random().nextInt(51)));
-                    teamRedRose.add(new Spearman(new Random().nextInt(20, 26), new Random().nextInt(51)));
+                    teamWhiteRose.add(new Spearman(Generator.numberXWhiteRose(), Generator.numberYSpearman()));
+                    teamRedRose.add(new Spearman(Generator.numberXRedRose(), Generator.numberYSpearman()));
                 }
                 case 6 -> {
-                    teamWhiteRose.add(new Robber(new Random().nextInt(0, 6), new Random().nextInt(51)));
-                    teamRedRose.add(new Robber(new Random().nextInt(20, 26), new Random().nextInt(51)));
+                    teamWhiteRose.add(new Robber(Generator.numberXWhiteRose(), Generator.numberYRobber()));
+                    teamRedRose.add(new Robber(Generator.numberXRedRose(), Generator.numberYRobber()));
                 }
             }
         }
 
-        int archers = 0;
-        int peasants = 0;
-
-        // чтобы не оказалось так, что лучники есть в командах, а крестьян им в помощь нет.
-        // в таком случае придется добавить по крестьянину в команды (по одиннадцатому игроку)
-
-        for (int i = 0; i < teamWhiteRose.size(); i++) {
-            if (teamWhiteRose.get(i) instanceof Crossbowman) {
-                archers++;
-            }
-            if (teamWhiteRose.get(i) instanceof Peasant) {
-                peasants++;
-            }
-        }
-
-        System.out.println("Лучников по " + archers + " в каждой из команд");
-        System.out.println("Крестьян по " + peasants + " в каждой из команд");
-
-        if ((archers > 0) && (peasants == 0)) {
-            teamWhiteRose.add(new Peasant(new Random().nextInt(0, 6), new Random().nextInt(51)));
-            teamRedRose.add(new Peasant(new Random().nextInt(20, 26), new Random().nextInt(51)));
-            System.out.println("Пришлось добавить по одному крестьянину в команды сверх нормы");
-        }
+        System.out.println("_".repeat(92));
+        System.out.println("Приветствуйте составы команд! Нечетные игроки - команда WhiteRose, четные - команда RedRose: ");
+        System.out.println("_".repeat(92));
 
         for (int i = 0; i < teamWhiteRose.size(); i++) {
             System.out.println(teamWhiteRose.get(i).getInfo());
             System.out.println(teamRedRose.get(i).getInfo());
         }
 
-        System.out.println("Общее количество игроков в каждой из команд в итоге составило " + teamWhiteRose.size());
-
-        ArrayList<Unit> commonList = new ArrayList<>(teamWhiteRose);
+        commonList.addAll(teamWhiteRose);
         commonList.addAll(teamRedRose);
 
         commonList.sort(reverseOrder(comparing(Unit -> Unit.attack)));
-        System.out.println("--------------------------------------------------------------");
+        Scanner in = new Scanner(System.in);
+        View.view();
+        in.nextLine();
+        System.out.println("_".repeat(63));
         System.out.println("Вывожу step персонажей в порядке убывания объема атакующих сил");
-        System.out.println("--------------------------------------------------------------");
+        System.out.println("_".repeat(63));
         commonList.forEach(n -> {
             String[] strings = n.getInfo().split(" ");
-            System.out.print("Для " + strings[1] + " ближайший враг и расстояние до него - ");
-            if (teamWhiteRose.contains(n)) n.step(teamRedRose,teamWhiteRose);
-            if (teamRedRose.contains(n)) n.step(teamWhiteRose,teamRedRose);
+            System.out.print(strings[0] + " " + strings[1] + " делает ход: ");
+            if (teamWhiteRose.contains(n)) n.step(teamRedRose, teamWhiteRose);
+            if (teamRedRose.contains(n)) n.step(teamWhiteRose, teamRedRose);
         });
 
 
-
     }
+
 }
