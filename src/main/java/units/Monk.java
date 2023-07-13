1cc90a12ed587d18d8ceb95bf42fd8f2eeff6833
Evgeniia Togochakova <lookatma@mail.ru>
2023-07-13 20:05:13 +0700

    Improved Monk.java

--- src/main/java/units/Monk.java
+++ src/main/java/units/Monk.java
@@ -1,6 +1,7 @@
 package units;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Monk extends Unit {
@@ -8,7 +9,7 @@ public class Monk extends Unit {
     int poison;
 
     public Monk(int x, int y) {
-        super(100, 50, new Random().nextInt(25,101),25, new int[]{5, 10, 15},x,y);
+        super(100, 50, new Random().nextInt(25, 101), 25, new int[]{5, 10, 15}, x, y);
         this.elixir = new Random().nextInt(30, 50);
         this.poison = new Random().nextInt(30, 50);
     }
@@ -19,11 +20,11 @@ public class Monk extends Unit {
     }
 
     public void healing(Unit patient) {
-        if (patient.currentHp < 50) {
-            System.out.println(patient.name + " был бы благодарен за лечение эликсиром");
-        }
         if (!healingAbility()) {
             System.out.println(name + " уже растратил свой эликсир!");
+        }
+        if (patient.currentHp >= 50) {
+            System.out.println(patient.name + " можно пока не лечить, жизненных сил у него на среднем уровне");
         }
         if (healingAbility() && patient.currentHp < 50) {
             int portion = (int) (this.elixir * 0.2);
@@ -35,7 +36,7 @@ public class Monk extends Unit {
 
     @Override
     protected boolean attackAbility() {
-        if (this.attack >= 5 && this.poison >=5) return true;
+        if (this.attack >= 5 && this.poison >= 5) return true;
         return false;
     }
 
@@ -57,14 +58,52 @@ public class Monk extends Unit {
 
 
     @Override
-    public void step(ArrayList<Unit> heroes,ArrayList<Unit> myOwnTeam) {
-        Unit closestVictim = findClosestEnemy(heroes);
-        System.out.println(closestVictim.name + " " + this.coordinates.distanceСalculation(closestVictim.coordinates));
+    public void step(ArrayList<Unit> heroes, ArrayList<Unit> myOwnTeam) {
+        if (this.currentHp > 0) {
+            System.out.print("Монах " + this.name + " жив...");
+            int digit = new Random().nextInt(2);
+            switch (digit) {
+                case 0 -> {
+                    Unit closestVictim = findClosestEnemy(heroes);
+                    System.out.println(closestVictim.name + " " + this.coordinates.distanceСalculation(closestVictim.coordinates));
+                    toAttack(closestVictim);
+                }
+                case 1 -> {
+                    float minHp = 100;
+                    for (var unit : myOwnTeam) {
+                        if (unit.currentHp < minHp) {
+                            minHp = unit.currentHp;
+                        }
+                    }
+                    if (minHp >= 50) {
+                        System.out.println("Монах " + this.name + " хотел было полечить, но соратники пока более-менее держатся, надо им помочь в атаке");
+                        Unit closestVictim = findClosestEnemy(heroes);
+                        System.out.println(closestVictim.name + " " + this.coordinates.distanceСalculation(closestVictim.coordinates));
+                        toAttack(closestVictim);
+                        return;
+                    }
+                    for (var unit : myOwnTeam) {
+                        if (unit.currentHp == minHp) {
+                            healing(unit);
+                            break;
+                        }
+                    }
+                }
+            }
+        } else {
+            System.out.println("Монах " + this.name + " мертв...");
+        }
     }
+
 
     @Override
     public String getInfo() {
-        return "Монах " + name + " жизненных сил: " + currentHp + " запас эликсира: " + elixir +
-                " запас яда: " + poison + " сил для распыления яда: " + attack;
+        return "Монах " + name + " hit points: " + currentHp + " эликсира: " + elixir +
+                " яда: " + poison + " сил травить: " + attack + " O:" + Arrays.toString(getCoordinates());
+    }
+
+    @Override
+    public String toString() {
+        return "O";
     }
 }
