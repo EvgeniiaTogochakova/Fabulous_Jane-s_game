109a1b9b4949e7e179f369fe6055c611274cd40a
Evgeniia Togochakova <lookatma@mail.ru>
2023-07-18 21:46:43 +0700

    Refined Crossbowman.java

--- src/main/java/units/Crossbowman.java
+++ src/main/java/units/Crossbowman.java
@@ -8,61 +8,56 @@ public class Crossbowman extends Unit {
     int arrows;
 
     public Crossbowman(int x, int y) {
-        super(100, 50, new Random().nextInt(25, 101), 25, new int[]{5, 10, 15}, x, y);
-        this.arrows = new Random().nextInt(10, 20);
-    }
-
-    protected boolean needArrows() {
-        if (this.arrows == 0) {
-            return true;
-        }
-        return false;
+        super(25, 15, new Random().nextInt(25, 101), 5, new int[]{5, 10, 15}, x, y);
+        this.arrows = new Random().nextInt(10, 15);
     }
 
 
     @Override
     public void toAttack(Unit target) {
-        this.attack -= 5;
         target.getDamageByArcher();
     }
 
     @Override
     public void step(ArrayList<Unit> heroes, ArrayList<Unit> myOwnTeam) {
-        Unit closestVictim = findClosestEnemy(heroes);
-        System.out.println(closestVictim.name + " " + this.coordinates.distanceСalculation(closestVictim.coordinates));
-        if (currentHp == 0) {
-            System.out.println("Арбалетчик " + name + " уже не живой!");
+        if (this.currentHp == 0) return;
+        ArrayList<Unit> liveOpponents = new ArrayList<>();
+        for (var liveOpponent : heroes) {
+            if (liveOpponent.currentHp > 0) liveOpponents.add(liveOpponent);
+        }
+//        System.out.println(closestVictim.name + " " + this.coordinates.distanceСalculation(closestVictim.coordinates));
+        if (liveOpponents.size() == 0) return;
+        Unit enemyTarget = findClosestEnemy(liveOpponents);
+        if (this.arrows > 0) {
+            toAttack(enemyTarget);
+            arrows--;
+            for (var unit : myOwnTeam) {
+                if (unit instanceof Peasant && unit.currentHp > 0 && unit.state == "Stand" && ((Peasant) unit).arrows > 0) {
+                    unit.state = "Busy";
+                    ((Peasant) unit).arrows--;
+                    this.arrows++;
+                    break;
+                }
+            }
+            state = "Shot";
             return;
         }
-        if (this.attack < 5) {
-            System.out.println("Арбалетчик " + name + " уже исчерпал свои атакующие силы!");
-            return;
-        }
-        if (needArrows()) {
-            System.out.println("Арбалетчик " + name + " стрелять не может, закончились стрелы");
-            return;
-        }
-        Unit enemyTarget = findClosestEnemy(heroes);
-        System.out.println("Арбалетчик " + name + " метко выстрелил в " + enemyTarget.name);
-        toAttack(enemyTarget);
-        System.out.println();
-        for (var unit : myOwnTeam) {
-            if (unit instanceof Peasant && unit.currentHp > 0 && unit.state == "Stand") {
-                unit.state = "Busy";
-                ((Peasant) unit).arrows--;
-                System.out.print("Арбалетчик " + this.name + " благодарит за стрелу крестьянина " + unit.name);
-                System.out.println(". " + unit.name + " становится Busy");
-                break;
+        if (this.arrows == 0) {
+            if (coordinates.distanceСalculation(enemyTarget.coordinates) < 2) {
+                toAttack(enemyTarget);
+                state = "Attack";
+            } else {
+                move(enemyTarget.coordinates, myOwnTeam);
+                state = "Moving";
             }
         }
-        this.arrows--;
     }
 
 
     @Override
     public String getInfo() {
-        return "Арбалетчик " + name + " hit points: " + currentHp + " стрел: " + arrows +
-                " сил стрелять: " + attack + " C:" + Arrays.toString(getCoordinates());
+        return "Арбалетчик " + name + " hp: " + currentHp + " стрел: " + arrows +
+                " initiative: " + attack + " C:" + Arrays.toString(getCoordinates()) + " " + state;
     }
 }
 
