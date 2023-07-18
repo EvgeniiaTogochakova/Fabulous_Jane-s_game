3ed1d6a64e3eaa3beb3aa0dcbedbe032b9abc103
Evgeniia Togochakova <lookatma@mail.ru>
2023-07-18 21:50:42 +0700

    Refined Sniper.java

--- src/main/java/units/Sniper.java
+++ src/main/java/units/Sniper.java
@@ -11,41 +11,9 @@ public class Sniper extends Crossbowman {
     }
 
     @Override
-    public void step(ArrayList<Unit> heroes, ArrayList<Unit> myOwnTeam) {
-        Unit closestVictim = findClosestEnemy(heroes);
-        System.out.println(closestVictim.name + " " + this.coordinates.distanceСalculation(closestVictim.coordinates));
-        if (currentHp == 0) {
-            System.out.println("Снайпер " + name + " уже не живой!");
-            return;
-        }
-        if (this.attack < 5) {
-            System.out.println("Снайпер " + name + " уже исчерпал свои атакующие силы!");
-            return;
-        }
-        if (needArrows()) {
-            System.out.println("Снайпер " + name + " стрелять не может, закончились стрелы");
-            return;
-        }
-        Unit enemyTarget = findClosestEnemy(heroes);
-        System.out.println("Снайпер " + name + " из западни неожиданно выстрелил из лука в мишень: " + enemyTarget.name);
-        toAttack(enemyTarget);
-        System.out.println();
-        for (var unit : myOwnTeam) {
-            if (unit instanceof Peasant && unit.currentHp > 0 && unit.state == "Stand") {
-                unit.state = "Busy";
-                ((Peasant) unit).arrows--;
-                System.out.print("Снайпер " + this.name + " благодарит за стрелу крестьянина " + unit.name);
-                System.out.println(". " + unit.name + " становится Busy");
-                break;
-            }
-        }
-        this.arrows--;
-    }
-
-    @Override
     public String getInfo() {
-        return "Снайпер " + name + " hit points: " + currentHp + " стрел: " + arrows +
-                " сил стрелять: " + attack + " N:" + Arrays.toString(getCoordinates());
+        return "Снайпер " + name + " hp: " + currentHp + " стрел: " + arrows +
+                " initiative: " + attack + " N:" + Arrays.toString(getCoordinates()) + " " + state;
     }
 
     @Override
