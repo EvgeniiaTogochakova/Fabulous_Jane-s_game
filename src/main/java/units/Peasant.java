cbfbdec271999a7d77abc8713fa8ff07e62abb5f
Evgeniia Togochakova <lookatma@mail.ru>
2023-07-18 21:50:04 +0700

    Refined Peasant.java

--- src/main/java/units/Peasant.java
+++ src/main/java/units/Peasant.java
@@ -8,56 +8,26 @@ public class Peasant extends Unit {
     int arrows;
 
     public Peasant(int x, int y) {
-        super(100, 50, 30, new int[]{5, 10, 15}, x, y);
-        this.arrows = new Random().nextInt(40, 50);
-    }
-
-
-    private boolean hasArrows() {
-        if (this.arrows > 0) {
-            return true;
-        }
-        return false;
-    }
-
-    public void givesArrows(Crossbowman archer) {
-        if (archer.needArrows()) {
-            System.out.println(archer.name + " просит стрелы");
-        }
-        if (!hasArrows()) {
-            System.out.println(this.name + " уже исчерпал свой запас стрел для помощи");
-        }
-        if (!archer.needArrows()) {
-            System.out.println("У " + archer.name + " еще не закончились стрелы");
-        }
-        if (archer.needArrows() && hasArrows()) {
-            archer.arrows += 1;
-            this.arrows -= 1;
-            System.out.println("Крестьянин " + name + " принес стрелу " + archer.name);
-        }
+        super(25, 15, 5, new int[]{5, 10, 15}, x, y);
+        this.arrows = new Random().nextInt(5, 10);
     }
 
     @Deprecated
-    public void toAttack(Unit target) {
-    }
+    public void toAttack(Unit target) {}
 
 
     @Override
     public void step(ArrayList<Unit> heroes, ArrayList<Unit> myOwnTeam) {
-        if (this.currentHp > 0) {
-            System.out.print("Крестьянин " + this.name + " жив... ");
-            if (state == "Busy") {
-                state = "Stand";
-                System.out.println("Крестьянин " + this.name + " изменил флаг с Busy на Stand");
-            }
+        if (this.currentHp == 0) return;
+        if (state == "Busy") {
+            state = "Stand";
         }
-        if (this.currentHp == 0) System.out.println("Крестьянин " + this.name + " мертв");
     }
 
 
     @Override
     public String getInfo() {
-        return "Крестьянин " + name + " hit points: " + currentHp + " стрел: " + arrows + " P:" + Arrays.toString(getCoordinates());
+        return "Крестьянин " + name + " hp: " + currentHp + " стрел: " + arrows + " P:" + Arrays.toString(getCoordinates()) + " " + state;
     }
 
 }
