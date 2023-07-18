3e4a892c35f9880c6e8a291fcd35a29c02c91061
Evgeniia Togochakova <lookatma@mail.ru>
2023-07-18 21:49:10 +0700

    Refined Generator.java

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -5,39 +5,13 @@ public class Generator {
 
 
     public static int numberXWhiteRose(){
-        return r.nextInt(1,6);
+        return r.nextInt(1,4);
     }
 
     public static int numberXRedRose() {
-        return r.nextInt(6, 11);
+        return r.nextInt(8, 11);
     }
 
-    public static int numberYMagician() {
-        return r.nextInt(1, 6);
-    }
-
-    public static int numberYMonk() {
-        return r.nextInt(2, 7);
-    }
-
-    public static int numberYPeasant() {
-        return r.nextInt(3, 8);
-    }
-
-    public static int numberYCrossbowman() {
-        return r.nextInt(1, 6);
-    }
-    public static int numberYSniper() {
-        return r.nextInt(6, 11);
-    }
-
-    public static int numberYSpearman() {
-        return r.nextInt(5, 10);
-    }
-
-    public static int numberYRobber() {
-        return r.nextInt(6, 11);
-    }
 
 }
 
