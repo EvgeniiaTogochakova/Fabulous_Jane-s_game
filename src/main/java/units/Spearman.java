a811e2d66ceb910ef19c0745644155cc56a92f3a
Evgeniia Togochakova <lookatma@mail.ru>
2023-07-18 21:51:02 +0700

    Refined Spearman.java

--- src/main/java/units/Spearman.java
+++ src/main/java/units/Spearman.java
@@ -7,18 +7,30 @@ import java.util.Random;
 public class Spearman extends Unit {
 
     public Spearman(int x, int y) {
-        super(100, 50, new Random().nextInt(25,101), 25, new int[]{1, 2, 3, 5, 9}, x,y);
+        super(25, 15, new Random().nextInt(25,101), 5, new int[]{1, 2, 3, 5, 9}, x,y);
     }
 
     @Override
     public void step(ArrayList<Unit> heroes,ArrayList<Unit> myOwnTeam) {
-        Unit closestVictim = findClosestEnemy(heroes);
-        System.out.println(closestVictim.name + " " + this.coordinates.distanceСalculation(closestVictim.coordinates));
+        if (this.currentHp == 0) return;
+        ArrayList<Unit> liveOpponents = new ArrayList<>();
+        for (var liveOpponent:heroes) {
+            if (liveOpponent.currentHp > 0 ) liveOpponents.add(liveOpponent);
+        }
+        if (liveOpponents.size() == 0) return;
+        Unit enemyToKill = findClosestEnemy(liveOpponents);
+        if (coordinates.distanceСalculation(enemyToKill.coordinates) < 2){
+            toAttack(enemyToKill);
+            state = "Attack";
+        }else{
+            move(enemyToKill.coordinates, myOwnTeam);
+            state = "Moving";
+        }
     }
 
     @Override
     public String getInfo() {
-        return "Копейщик " + name + " hit points: " + currentHp + " сил для удара : " + attack + " S:" + Arrays.toString(getCoordinates());
+        return "Копейщик " + name + " hp: " + currentHp + " initiative : " + attack + " S:" + Arrays.toString(getCoordinates()) + " " + state;
     }
 
 }
