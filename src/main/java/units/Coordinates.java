bb127cde14a81502b4ee3e3d633f531fc0138395
Evgeniia Togochakova <lookatma@mail.ru>
2023-07-18 21:46:19 +0700

    Refined Coordinates.java

--- src/main/java/units/Coordinates.java
+++ src/main/java/units/Coordinates.java
@@ -1,5 +1,7 @@
 package units;
 
+import java.util.ArrayList;
+
 import static java.lang.Math.round;
 
 public class Coordinates {
@@ -14,5 +16,27 @@ public class Coordinates {
         int xDistance = coordinates.x - x;
         int yDistance = coordinates.y - y;
         return Math.round(Math.sqrt(Math.pow(xDistance,2) + Math.pow(yDistance,2)));
+    }
+
+    public Coordinates newPosition(Coordinates targetEnemyPosition){
+        Coordinates currentPosition = new Coordinates(x,y);
+        if (Math.abs(targetEnemyPosition.x - x) >= Math.abs(targetEnemyPosition.y - y)){
+            if (targetEnemyPosition.x - x > 0) currentPosition.x ++;
+            else currentPosition.x--;
+        }
+
+        if (Math.abs(targetEnemyPosition.x - x) < Math.abs(targetEnemyPosition.y - this.y)){
+            if (targetEnemyPosition.y - y > 0) currentPosition.y ++;
+            else currentPosition.y--;
+        }
+
+        return currentPosition;
+    }
+
+    public boolean containsByPosition(Coordinates potentialNewPosition, ArrayList<Unit> allies) {
+        for (var unit : allies){
+            if (unit.coordinates == potentialNewPosition) return true;
+        }
+        return false;
     }
 }
